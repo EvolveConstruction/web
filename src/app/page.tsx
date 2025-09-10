fa446f2af8f91db7816893528f148e4b07c6faf0
Eddie <eddiebu.1515@gmail.com>
2025-09-09 21:58:52 -0500

    Show website construction message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Typography, Box } from "@mui/material";
 import { CSSProperties } from "react";
+import LoadingVideo from "@/components/loadingVideo";
 
 const containerBoxStyle: CSSProperties = {
   display: "flex",
@@ -12,7 +13,8 @@ export default function Home() {
   return (
     <Box sx={containerBoxStyle}>
       <Typography variant="h2">Website under construction...</Typography>
-      <Typography variant="h5">Check back soon!</Typography>
+      <Typography variant="h6">Please check back soon!</Typography>
+      <LoadingVideo />
     </Box>
   );
 }
---
+++ src/components/loadingVideo.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+
+export default function LoadingVideo() {
+    return (
+        <>
+        <DotLottieReact
+      src="https://lottie.host/ea75c810-a2d6-46e4-ae7e-6203f7af7e5d/ij1PKX4UBI.lottie"
+      autoplay
+      loop
+      />
+        </>
+    );
+}
